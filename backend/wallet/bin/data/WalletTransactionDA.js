8c97a8a8e06bd9ab40ac6d79a19288e81b72c928
Daniel Tobón Uribe <daniel.tobon@nebulae.com.co>
2022-02-22 15:02:55 -0500

    Se agrega variable para el valor previo de la billetera en la transaccion

--- backend/wallet/bin/data/WalletTransactionDA.js
+++ backend/wallet/bin/data/WalletTransactionDA.js
@@ -29,7 +29,6 @@ class WalletTransactionDA {
    * @param {*} transactionData transaction to create
    */
   static saveTransactionHistory$(transactionData, walletPrev) {
-    console.log("walletPrev ===> ", walletPrev)
     const collection = mongoDB
       .getHistoricalDbByYYMM(transactionData._id.split("-").pop())
       .collection(COLLECTION_NAME);
